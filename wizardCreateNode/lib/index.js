d7aa6f825eeef5bf3f6e6c47858a19225492bdf9
alexstrelnyk <alex.strelnyk@gmail.com>
2025-08-14 15:27:29 +0300

    Removed main grid and make "Add Node" popup opened on startup

--- wizardCreateNode/lib/index.js
+++ wizardCreateNode/lib/index.js
@@ -1,35 +1,6 @@
 Ext.onReady(function () {
   Ext.QuickTips.init();
 
-  function refreshGrid() {
-
-    Ext.Ajax.request({
-      url: './tools/wizardCreateNode/src/index.php',
-      params: { action: 'get_data' },
-      success: function (result) {
-        const res = Ext.decode(result.responseText);
-        if (res.success && res.data) {
-
-          const grid = Ext.getCmp('grid_cr_node');
-          if (grid) {
-            const store = grid.getStore();
-            store.removeAll();
-            (res.data.values || []).forEach(item => {
-              store.add(new store.recordType({
-                NNAME: item.NNAME,
-                NODETYPE: item.NODETYPE,
-                NODEDEF: item.NODEDEF,
-                SUBTYPE: item.SUBTYPE,
-                LNAME: item.LNAME,
-                COMMENTS: item.COMMENTS
-              }));
-              ;
-            });
-          }
-        }
-      }
-    });
-  }
   function openEditWindow() {
     let isChanged = false;
 
@@ -55,7 +26,6 @@ Ext.onReady(function () {
             if (res.success) {
               Ext.Msg.alert('Success', 'Node successfully created');
               form.ownerCt.close();
-              refreshGrid();
             } else {
               Ext.Msg.alert('Error', res.message || 'Failed to save node');
             }
@@ -91,16 +61,18 @@ Ext.onReady(function () {
       anchor: '95%'
     });
 
+    const nodeDefStore = new Ext.data.JsonStore({
+      url: './tools/wizardCreateNode/src/index.php',
+      baseParams: { action: 'get_node_defs' },
+      root: 'data',
+      fields: ['ID', 'NAME']
+    });
+
     const nodeDefCombo = new Ext.form.ComboBox({
       fieldLabel: 'Node Def',
       name: 'NODEDEF',
       hiddenName: 'NODEDEF',
-      store: new Ext.data.JsonStore({
-        url: './tools/wizardCreateNode/src/index.php',
-        baseParams: { action: 'get_node_defs' },
-        root: 'data',
-        fields: ['ID', 'NAME']
-      }),
+      store: nodeDefStore,
       valueField: 'ID',
       displayField: 'NAME',
       mode: 'remote',
@@ -110,8 +82,21 @@ Ext.onReady(function () {
       forceSelection: true,
       typeAhead: false,
       allowBlank: false,
-      anchor: '95%'
+      anchor: '95%',
+      listeners: {
+        expand: function (combo) {
+          const nodeTypeVal = nodeTypeCombo.getValue();
+          if (!nodeTypeVal) {
+            Ext.Msg.alert('Warning', 'Please select Node Type first.');
+            combo.collapse();
+            return;
+          }
+          combo.store.baseParams.node_type = nodeTypeVal;
+          combo.store.load();
+        }
+      }
     });
+
 
     const subTypeCombo = new Ext.form.ComboBox({
       fieldLabel: 'Subtype',
@@ -210,67 +195,11 @@ Ext.onReady(function () {
 
   window.wizard_create_node = Ext.extend(Ext.Window, {
     constructor: function (cfg) {
-      const config = Ext.applyIf(cfg || {}, {
-        title: 'Create Node Wizard',
-        width: 740,
-        autoHeight: true,
-        layout: 'form',
-        border: false,
-        plain: true,
-        modal: true,
-        resizable: false,
-        closable: true,
-        bodyStyle: 'padding: 10px;',
-        items: [
-          {
-            xtype: 'panel',
-            layout: 'hbox',
-            border: false,
-            bodyStyle: 'margin: 10px 0;',
-            items: [
-              {
-                id: 'grid_cr_node',
-                xtype: 'grid',
-                height: 150,
-                store: new Ext.data.ArrayStore({
-                  fields: ['NNAME', 'NODETYPE', 'NODEDEF', 'SUBTYPE', 'LNAME', 'COMMENTS'],
-                  data: []
-                }),
-                columns: [
-                  { header: 'Name', dataIndex: 'NNAME', width: 200 },
-                  { header: 'Node Type', dataIndex: 'NODETYPE', width: 100 },
-                  { header: 'Node Def', dataIndex: 'NODEDEF', width: 140 },
-                  { header: 'Subtype', dataIndex: 'SUBTYPE', width: 140 },
-                  { header: 'Location', dataIndex: 'LNAME', width: 80 },
-                  { header: 'Comments', dataIndex: 'COMMENTS', width: 80 }
-                ]
-              }
-            ]
-          }
-
-        ],
-        buttons: [
-          {
-            text: 'Add',
-            handler: function () {
-              openEditWindow();
-            }
-          }, {
-            text: 'Cancel',
-            handler: function () {
-              this.ownerCt.ownerCt.close();
-            }
-          }
-        ]
-      });
+      const config = Ext.applyIf(cfg || {}, openEditWindow());
 
       window.wizard_create_node.superclass.constructor.call(this, config);
 
-      this.initWizard = function (cfg) {
-        refreshGrid();
-
-        this.show();
-      };
+      this.initWizard = function (cfg) { };
 
 
     }
