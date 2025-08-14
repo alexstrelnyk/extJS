8b7ddf207e404b4f90ae20c6ac671fac8898df7f
alexstrelnyk <alex.strelnyk@gmail.com>
2025-08-14 15:59:05 +0300

    Fixed setAttributes()

--- wizardCreateNode/src/index.php
+++ wizardCreateNode/src/index.php
@@ -34,23 +34,79 @@ function getSQLData($con, $sql, $get_single = false)
 	return $ar;
 }
 
+function addAttributes($db_cfg, $obj_id, $name, $value)
+{
+	//	echo __FILE__.' '.__LINE__.'<pre>';print_r($obj_id.$name.$value).'</pre>';die;
+	$sql = "
+		DECLARE
+    o_errorcode            NUMBER;
+    o_errortext            VARCHAR2(4000);
+	";
+	$sql .= "i_dimobject   NUMBER := 1; 
+			";
+	$sql .= "i_objectid   NUMBER := " . $obj_id . "; 
+			";
+	$sql .= "i_attribute   VARCHAR2(200) := '" . $name . "'; 
+			";
+	$sql .= "i_value   VARCHAR2(200) := '" . $value . "'; 
+			";
+
+	$sql .= "
+
+BEGIN
+
+  CRAMER.getsession();
+  
+  PKGGeneral.SetObjectAttribute(
+	o_errorcode, 
+	o_errortext, 
+	i_dimobject, 
+	i_objectid, 
+	i_attribute, 
+	i_value
+  );
+
+  IF o_errorcode != 0 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Package Error: ' || o_errortext);
+  END IF;
+
+  :ERRCODE := o_errorcode;
+  :ERRTEXT := o_errortext;
+
+END;
+
+";
+
+	//	exit($sql);
+
+	//	echo __FILE__.' '.__LINE__.'<pre>';print_r($db_cfg).'</pre>';die;
+
+	$pcon = $db_cfg['cramer_admin'];
+
+	$con = oci_connect($pcon['login'], $pcon['pass'], $pcon['param']);
+	$st = oci_parse($con, $sql);
+
+	oci_bind_by_name($st, ':ERRCODE', $errorcode, 10);
+	oci_bind_by_name($st, ':ERRTEXT', $errortext, 4000);
+
+	if (!oci_execute($st)) {
+		$e = oci_error($st);
+		echo json_encode(['success' => false, 'message' => 'Oracle Error: ' . $e['message']]);
+		oci_rollback($con);
+		exit;
+	}
+
+	if ($errorcode != 0) {
+		echo json_encode(['success' => false, 'message' => 'Procedure Error: ' . $errortext]);
+		oci_rollback($con);
+		exit;
+	}
+
+	oci_commit($con);
+	//	exit;
+}
+
 switch ($action) {
-	case 'get_data':
-		$ar['values'] = getSQLData($con, "
-select 
-n.name nname, 
-n.node2nodetype nodetype, 
-n.node2nodedef nodedef, 
-n.subtype, 
-l.name lname, 
-n.notes comments 
-from CRAMER.node_o n
-JOIN CRAMER.location_o l ON l.locationid = n.node2location
-and rownum < 20
-ORDER BY n.nodeid DESC
-");
-		exit(json_encode(array('success' => true, 'data' => $ar)));
-		break;
 	case 'get_location':
 		$sql = "select distinct(l.name), l.locationid id from CRAMER.location_o l 
 		WHERE rownum < 20 ";
@@ -96,11 +152,14 @@ ORDER BY n.nodeid DESC
 		if (isset($_REQUEST['query']) && $query = $_REQUEST['query']) {
 			$sql .= "AND nd.name LIKE '%" . $query . "%' ";
 		}
+		if (isset($_REQUEST['node_type']) && $query = $_REQUEST['node_type']) {
+			$sql .= "AND nd.nodedef2nodetype = " . $query;
+		}
 
 		sendJSONFromSQL($con, $sql, false);
 		break;
 	case 'save_node':
-
+		//	addAttributes($db_cfg, '3458805', 'NOTES', 'test comment');
 		$sql = "
 		DECLARE
     o_errorcode            NUMBER;
@@ -141,6 +200,7 @@ BEGIN
 
   :ERRCODE := o_errorcode;
   :ERRTEXT := o_errortext;
+  :NODEID := o_nodeid;
 
 END;
 
@@ -152,6 +212,8 @@ END;
 
 		$con = oci_connect($pcon['login'], $pcon['pass'], $pcon['param']);
 		$st = oci_parse($con, $sql);
+
+		oci_bind_by_name($st, ':NODEID', $node_id, 10);
 		oci_bind_by_name($st, ':ERRCODE', $errorcode, 10);
 		oci_bind_by_name($st, ':ERRTEXT', $errortext, 4000);
 
@@ -167,6 +229,9 @@ END;
 			oci_rollback($con);
 			exit;
 		}
+
+		addAttributes($db_cfg, $node_id, 'NOTES', $_REQUEST['COMMENTS']);
+		//	exit();
 
 		oci_commit($con);
 		echo json_encode(['success' => true, 'data' => []]);
